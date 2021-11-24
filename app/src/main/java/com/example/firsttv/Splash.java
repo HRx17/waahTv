9f069b87fa190b5d60ede517724ce3e3cd395161
Haet17 <haetranpariya1789@gmail.com>
2021-11-25 01:38:34 +0530

    Commit1

--- app/src/main/java/com/example/firsttv/Splash.java
+++ app/src/main/java/com/example/firsttv/Splash.java
@@ -2,6 +2,7 @@ package com.example.firsttv;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -9,17 +10,23 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 
 import com.example.firsttv.ui.MainActivity;
+
+import java.util.Date;
 
 public class Splash extends FragmentActivity {
 
     ImageView imageView;
     ProgressBar progressBar;
     Handler handler = new Handler();
+    String tmp = "1";
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +40,39 @@ public class Splash extends FragmentActivity {
         Animation animFadeIn = AnimationUtils.loadAnimation(this, R.anim.fadein);
 
         SharedPreferences sharedPreferences = getSharedPreferences("time", 0);
-        String tmp1 = sharedPreferences.getString("time", null);
 
+        Date today = new Date();
+        Toast.makeText(this, today.toString(), Toast.LENGTH_SHORT).show();
+        System.out.println(today);
+        String d,m,y;
+        d = String.valueOf(today.getDay());
+        m = String.valueOf(today.getMonth());
+        y = String.valueOf(today.getYear());
+        String tmp1 = sharedPreferences.getString("time", null);
+        String dd = String.valueOf(tmp1.subSequence(0,2));
+        String mm = String.valueOf(tmp1.subSequence(3,5));
+        String yy = String.valueOf(tmp1.subSequence(6,9));
+        int date= Integer.valueOf(dd);
+        int month= Integer.valueOf(mm);
+        int year= Integer.valueOf(yy);
+        int dt= Integer.valueOf(d);
+        int mt= Integer.valueOf(m);
+        int yr= Integer.valueOf(y);
+
+
+        if(year > yr){
+            tmp = null;
+        }
+        else{
+            if(month > mt){
+                tmp=null;
+            }
+            else if(mm.equals(m)){
+                if(date>dt){
+                    tmp=null;
+                }
+            }
+        }
 
         animFadeIn.reset();
         imageView.clearAnimation();
@@ -51,7 +89,7 @@ public class Splash extends FragmentActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                if(tmp1 != null){
+                if(tmp != null){
                     Intent intent = new Intent(Splash.this, MainActivity.class);
                     startActivity(intent);
                 }
